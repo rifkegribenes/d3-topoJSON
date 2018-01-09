a343a52d08a1ab9e2471b3411f12f849df1c3e7e
Sarah Schneider <rifkegribenes@gmail.com>
2018-01-08 16:30:01 -0800

    resizeable mercator

--- script.js
+++ script.js
@@ -1,125 +1,75 @@
-// source code for creating orthographic projection with rotate on click:
-// https://bl.ocks.org/austinczarnecki/fe80afa64724c9630930
+// const width = window.innerWidth;
+// const height = window.innerHeight;
 
-const width = 950;
-const height = 700;
+// const svg = d3.select("body").append("svg")
 
-const colors = { clickable: 'darkgreen', hover: 'green', clicked: "mediumvioletred", clickhover: "deeppink" };
+// const projection = d3.geoMercator()
+//   .scale(width / 2 / Math.PI)
+//   .translate([width / 2, height / 2])
 
-const projection = d3.geoOrthographic()
-    .scale(300)
-    .translate([width / 2, height / 2])
-    .clipAngle(90)
-    .precision(10);
+// const path = d3.geoPath()
+//   .projection(projection);
 
-const path = d3.geoPath()
-    .projection(projection);
+// const url = "http://enjalot.github.io/wwsd/data/world/world-110m.geojson";
+// d3.json(url, function(err, geojson) {
+//   svg.append("path")
+//     .attr("d", path(geojson))
+// })
 
-const graticule = d3.geoGraticule();
 
-const map = d3.select("body").append("svg")
+/////
+
+
+
+
+let width = window.innerWidth * .97;
+let height = width / 2;
+const svg = d3.select("body").append("svg")
+
+//Define map projection
+const projection = d3.geoMercator()
+      .translate([780,360])
+      .scale(300);
+
+//Define path generator
+const path = d3.geoPath().projection(projection);
+
+//Resize function
+const resize = () => {
+  width = window.innerWidth * .97;
+  height = width / 2;
+
+  d3.selectAll("path").attr("transform", `scale(${width / 1900 })`);
+  svg
     .attr("width", width)
-    .attr("height", height)
-    .attr("class", "map");
+    .attr("height", height);
 
-map.append("defs").append("path")
-    .datum({type: "Sphere"})
-    .attr("id", "sphere")
-    .attr("d", path);
+  // projection
+  //   .translate([780,360])
+  //   .scale(300);
 
-map.append("use")
-    .attr("class", "stroke")
-    .attr("xlink:href", "#sphere");
+  //  d3.select("svg").attr("width", width).attr("height", height);
 
-map.append("use")
-    .attr("class", "fill")
-    .attr("xlink:href", "#sphere");
+  //  d3.selectAll("path").attr('d', path);
+}
 
-map.append("path")
-    .datum(graticule)
-    .attr("class", "graticule")
-    .attr("d", path);
+const url = "http://enjalot.github.io/wwsd/data/world/world-110m.geojson";
 
-d3.queue()
-    .defer(d3.json, "https://unpkg.com/world-atlas@1/world/110m.json")
-    .defer(d3.tsv, "countrynames.tsv")
-    .await(ready);
+ d3.json(url, (err, geojson) => {
 
-function ready(error, world, names) {
-  if (error) throw error;
+  svg
+    .attr("width", width)
+    .attr("height", height);
 
-  var globe = {type: "Sphere"},
-    land = topojson.feature(world, world.objects.land),
-    countries = topojson.feature(world, world.objects.countries).features,
-    borders = topojson.mesh(world, world.objects.countries, function(a, b) { return a !== b; });
+  //draw map
+  const map = svg.selectAll("path")
+    .data(geojson.features)
+    .enter()
+    .append("path")
+    .attr("d", path(geojson))
+    .style("fill", "#3498db");
+  })
 
-  countries = countries.filter(function(d) {
-    return names.some(function(n) {
-      if (d.id == n.id) return d.name = n.name;
-    });
-  }).sort(function(a, b) {
-    return a.name.localeCompare(b.name);
-  });
 
-  map.insert("path", ".graticule")
-      .datum(topojson.feature(world, world.objects.land))
-      .attr("class", "land")
-      .attr("d", path);
+d3.select(window).on('resize', resize);
 
-  for(i = 0; i < names.length; i++) {
-    for (j = 0; j < countries.length; j++) {
-      if (countries[j].id == names[i].id) {
-        map.insert("path", ".graticule")
-          .datum(countries[j])
-          .attr("fill", colors.clickable)
-          .attr("d", path)
-          .attr("class", "clickable")
-          .attr("data-country-id", j)
-          .on("click", function() {
-            d3.selectAll(".clicked")
-              .classed("clicked", false)
-              .attr("fill", colors.clickable);
-            d3.select(this)
-              .classed("clicked", true)
-              .attr("fill", colors.clicked);
-
-            (function transition() {
-              d3.select(".clicked").transition()
-              .duration(1250)
-              .tween("rotate", function() {
-                var p = d3.geo.centroid(countries[d3.select(this).attr("data-country-id")]),
-                    r = d3.interpolate(projection.rotate(), [-p[0], -p[1]]);
-                return function (t) {
-                  projection.rotate(r(t));
-                  map.selectAll("path").attr("d", path);
-                }
-              });
-            })();
-          })
-          .on("mousemove", function() {
-            var c = d3.select(this);
-            if (c.classed("clicked")) {
-              c.attr("fill", colors.clickhover);
-            } else {
-              c.attr("fill", colors.hover);
-            }
-          })
-          .on("mouseout", function() {
-            var c = d3.select(this);
-            if (c.classed("clicked")) {
-              c.attr("fill", colors.clicked);
-            } else {
-              d3.select(this).attr("fill", colors.clickable);
-            }
-          });
-      }
-    }
-  }
-
-  map.insert("path", ".graticule")
-      .datum(topojson.mesh(world, world.objects.countries, function(a, b) { return a !== b; }))
-      .attr("class", "boundary")
-      .attr("d", path);
-};
-
-d3.select(self.frameElement).style("height", height + "px");
