4de4d0603239556be3afe0935e83527d76a65324
Sarah Schneider <rifkegribenes@gmail.com>
2018-01-09 22:36:51 -0800

    zoomable mercator with country outlines and impact circles

--- script.js
+++ script.js
@@ -1,81 +1,62 @@
-// const width = window.innerWidth;
-// const height = window.innerHeight;
+const width = window.innerWidth;
+const height = width / 2;
+const tooltip = d3.select('body').append('div').attr('class', 'tooltip');
 
-// const svg = d3.select("body").append("svg")
+const svg = d3.select('body').append('svg')
+  .attr('width', width)
+  .attr('height', height);
 
-// const projection = d3.geoMercator()
-//   .scale(width / 2 / Math.PI)
-//   .translate([width / 2, height / 2])
-
-// const path = d3.geoPath()
-//   .projection(projection);
-
-// const url = "http://enjalot.github.io/wwsd/data/world/world-110m.geojson";
-// d3.json(url, function(err, geojson) {
-//   svg.append("path")
-//     .attr("d", path(geojson))
-// })
-
-
-/////
-
-
-
-
-let width = window.innerWidth;
-let height = width / 2;
-const svg = d3.select("body").append("svg")
-
-//Define map projection
 const projection = d3.geoMercator()
-      .translate([780,360])
-      .scale(300);
+  .translate([780,360])
+  .scale(300);
 
-//Define path generator
 const path = d3.geoPath().projection(projection);
+const borders = svg.append("g")
+  .attr('width', width)
+  .attr('height', height)
+  // .append('rect')
+  // .attr("width", width)
+  // .attr("height", height)
+  // .style("fill", "#4196f6");
 
-//Resize function
-const resize = () => {
-  width = window.innerWidth;
-  height = width / 2;
+d3.queue()
+  .defer(d3.json, 'https://unpkg.com/world-atlas@1/world/110m.json')
+  .defer(d3.json, 'https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/meteorite-strike-data.json')
+  .await((error, world, meteorites) => {
 
-  d3.selectAll("path").attr("transform", `scale(${width / 1900 })`);
-  svg
-    .attr("width", width)
-    .attr("height", height);
+    borders.selectAll("path")
+      .data(topojson.feature(world, world.objects.countries)
+          .features)
+      .enter()
+      .append("path")
+      .attr("d", path)
+      .attr("class", "border")
 
+    svg.append('g')
+      .selectAll('.meteorite')
+      .data(meteorites.features)
+      .enter().append('path')
+        .attr('class', 'meteorite')
+        .attr('d', path.pointRadius((d) => Math.cbrt(d.properties.mass * 3 / (4 * Math.PI)) / 10 ))
+        .attr('fill', 'red')
+        .style('opacity', '0.5')
+        .on('mouseover', (d) => {
+          tooltip.transition()
+            .duration(100)
+            .style('opacity', .9);
+          tooltip.text(`${(new Date(d.properties.year)).getFullYear()} ${d.properties.name} ${d.properties.mass}kg`)
+            .style('left', `${d3.event.pageX - 87}px`)
+            .style('top', `${d3.event.pageY - 80}px`);
+        })
+        .on('mouseout', () => {
+          tooltip.transition()
+          .duration(400)
+          .style('opacity', 0);
+        });
+})
 
-  // projection
-  //   .translate([780,360])
-  //   .scale(300);
+const zoom = d3.zoom()
+  .scaleExtent([0.5, 10])
+  .on('zoom', () => d3.selectAll('g').attr('transform', d3.event.transform));
 
-  //  d3.select("svg").attr("width", width).attr("height", height);
-
-  //  d3.selectAll("path").attr('d', path);
-}
-
-const url = "https://unpkg.com/world-atlas@1/world/110m.json";
-
- d3.json(url, (err, world) => {
-
-  svg
-    .attr("width", width)
-    .attr("height", height)
-    .append('rect')
-    .attr("width", width)
-    .attr("height", height)
-    .style("fill", "#4196f6");
-
-  //draw map
-  const map = svg.selectAll("path")
-    .data(topojson.feature(world, world.objects.countries).features)
-    .enter()
-    .append('path')
-    .attr('fill', '#ddd')
-    .attr('stroke', '#266D98')
-    .attr('d', path);
-  })
-
-
-d3.select(window).on('resize', resize);
-
+svg.call(zoom);
