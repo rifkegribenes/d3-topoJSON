caf003426fb22b5ea456edd426dfb2a153f9576b
Sarah Schneider <rifkegribenes@gmail.com>
2018-01-09 22:54:19 -0800

    added date and color scales

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const width = window.innerWidth;
 const height = width / 2;
 const tooltip = d3.select('body').append('div').attr('class', 'tooltip');
+const metorScale = d3.scalePow().exponent(.5).domain([0, 1000, 10000, 56000, 23000000]);
+
+const colorScale = d3.scaleLinear().domain([1400, 1800, 1860, 1940, 2015]);
 
 const svg = d3.select('body').append('svg')
   .attr('width', width)
@@ -24,6 +27,12 @@ d3.queue()
   .defer(d3.json, 'https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/meteorite-strike-data.json')
   .await((error, world, meteorites) => {
 
+    metorScale
+    .range([2.5, 3, 4, 5, 10]);
+
+    colorScale
+    .range(["#FFFF66", "#FFFF00", "#E68000", "#D94000", "#CC0000"]);
+
     borders.selectAll("path")
       .data(topojson.feature(world, world.objects.countries)
           .features)
@@ -35,10 +44,18 @@ d3.queue()
     svg.append('g')
       .selectAll('.meteorite')
       .data(meteorites.features)
-      .enter().append('path')
+      .enter().append('circle')
         .attr('class', 'meteorite')
-        .attr('d', path.pointRadius((d) => Math.cbrt(d.properties.mass * 3 / (4 * Math.PI)) / 10 ))
-        .attr('fill', 'red')
+        .attr('cx', d => projection([d.properties.reclong, d.properties.reclat])[0])
+        .attr('cy', d => projection([d.properties.reclong,d.properties.reclat])[1])
+        // .attr("cx", d => projection([d.long, d.lat])[0])
+        // .attr("cy", d => projection([d.long, d.lat])[1])
+        .attr("r",  d => metorScale(d.properties.mass))
+        .attr("id", d => `id${d.id}`)
+        .attr('fill', d => {
+          const year = (new Date(d.properties.year)).getFullYear();
+          return colorScale(year)
+        })
         .style('opacity', '0.5')
         .on('mouseover', (d) => {
           tooltip.transition()
